34bdda9a9d5cc6216a5df592ca94f7800d548361
DanteDeRuwe <dantederuwe@gmail.com>
2020-07-14 20:11:04 +0200

    Add project files

---
+++ sketch.js
@@ -0,0 +1,10 @@
+let canvas;
+
+function setup() {
+  canvas = createCanvas(document.getElementById("canvas").clientWidth, document.getElementById("canvas").clientHeight);
+  canvas.parent("canvas");
+}
+
+function draw() {
+  background(0);
+}
